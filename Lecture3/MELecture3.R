4f317a28b99605d26cbb08ba650800b38303f47c
cbw1243 <bwchen0719@gmail.com>
2018-08-13 16:34:54 -0500

    Update lecture 3

---
+++ Lecture3/MELecture3.R
@@ -0,0 +1,22 @@
+# Lecture 3: Calculus I
+# Linear approximation of functions.
+
+# Plot functions. 
+x <- seq(2, 3, by = 0.01)
+
+# Actual function
+y1 <- sapply(x, function(i) 2*log(i))   # y = 2x^.5
+plot(x, y1, type = 'l', col = 'purple', lwd = 3, ylim = c(1, 3), xlab = 'x', ylab = 'y')
+
+# Approximated function
+y2 <- sapply(x, function(i) 2*log(2) + i - 2)  # y = 2x^2 - 4x + 1 
+lines(x, y2, col = 'red', lwd = 3)
+legend('topleft', legend=c("Actual", "Approximated"),
+       col=c("purple", 'red'), 
+       lwd = 3, cex = 0.8, ncol = 1)
+
+# Higher-order approximition
+y3 <- sapply(x, function(i) 2*log(2) + (i - 2) - (2^(-2))*((i-2)^2))  
+lines(x, y3, col = 'blue', lwd = 3)
+
+

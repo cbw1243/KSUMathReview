83acc65d7b04e5b10dbaf152d449cde8c99a7153
cbw1243 <bwchen0719@gmail.com>
2018-08-22 21:54:34 -0500

    Updates on time in syllabus and an additional note

--- Lecture3/MELecture3.R
+++ Lecture3/MELecture3.R
@@ -5,11 +5,11 @@
 x <- seq(2, 3, by = 0.01)
 
 # Actual function
-y1 <- sapply(x, function(i) 2*log(i))   # y = 2x^.5
+y1 <- sapply(x, function(i) 2*log(i))   # y = 2log(x)
 plot(x, y1, type = 'l', col = 'purple', lwd = 3, ylim = c(1, 3), xlab = 'x', ylab = 'y')
 
 # Approximated function
-y2 <- sapply(x, function(i) 2*log(2) + i - 2)  # y = 2x^2 - 4x + 1 
+y2 <- sapply(x, function(i) 2*log(2) + i - 2)  # y = 2log(2) - (x-2) 
 lines(x, y2, col = 'red', lwd = 3)
 legend('topleft', legend=c("Actual", "Approximated"),
        col=c("purple", 'red'), 
